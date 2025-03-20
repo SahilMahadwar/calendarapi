0cb2ceedbef9cd2eb4389f6e7e830eb856ba259f
Sahil Mahadwar <accounts@mahadwar.com>
2025-03-20 22:16:14 +0530

    add use-calendar

---
+++ apps/app/src/hooks/use-calendar.tsx
@@ -0,0 +1,43 @@
+import { axiosApiInstance } from "@/libs/axios-api-Instance";
+import { queryClient } from "@/libs/react-query/query-client";
+import { ApiResponse, GoogleTokenResponse } from "@/types/api-res";
+import { CalendarEventInput } from "@/types/common";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { useAuth } from "./use-auth";
+
+export const useCalendar = () => {
+  const { getTokens } = useAuth();
+
+  const { access_token, expiry_date, refresh_token } = getTokens();
+
+  const getCalendarEvents = useQuery({
+    queryFn: async () => {
+      return axiosApiInstance.get<ApiResponse<GoogleTokenResponse>>(
+        `/calendar/events?access_token=${access_token}&refresh_token=${refresh_token}&expiry_date=${expiry_date}`
+      );
+    },
+
+    queryKey: ["calendar"],
+  });
+
+  const createCalendarEvent = useMutation({
+    mutationFn: async (event: CalendarEventInput) => {
+      return axiosApiInstance.post<ApiResponse<CalendarEventInput>>(
+        "/calendar/events",
+        {
+          access_token,
+          refresh_token,
+          expiry_date,
+          event: event,
+        }
+      );
+    },
+    onSuccess: ({ data }) => {
+      queryClient.invalidateQueries({
+        queryKey: ["calendar"],
+      });
+    },
+  });
+
+  return { getCalendarEvents, createCalendarEvent };
+};
